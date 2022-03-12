248a2f67ccf8fedb74c6c6a4278332268779bd39
NdaaXD <98279739+NdaaXD@users.noreply.github.com>
2022-03-12 07:51:26 +0700

    Update main.js

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ let rl = Readline.createInterface(process.stdin, process.stdout)
 let WAConnection = simple.WAConnection(_WAConnection)
 
 
-global.owner = ['6285294959195'] // Put your number here
+global.owner = ['6288239928939'] // Put your number here
 global.mods = [] // Want some help?
 global.prems = ['6285294959195'] // Premium user has unlimited limit
 global.APIs = { // API Prefix
